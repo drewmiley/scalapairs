c8588651f2636dc9c187a729585b20049217b2cd
Drew Miley <drew.miley@opencastsoftware.com>
2018-07-03 11:01:58 +0100

    spacesRequiredForPairs should use fold rather than var

--- src/main/scala/PairsSolitare.scala
+++ src/main/scala/PairsSolitare.scala
@@ -2,14 +2,14 @@ package main.scala
 
 class PairsSolitare(shuffledDeck: Seq[Int], spaces: Int) {
 
+    case class State(activeCards: Set[Int] = Set(), spacesUsed: Int = 0)
+
     def spacesRequiredForPairs(shuffledDeck: Seq[Int]): Int = {
-        var activeCards = Set[Int]()
-        var spacesUsed = 0
-        shuffledDeck.foreach((i: Int) => {
-            activeCards = if (activeCards(i)) activeCards - i else activeCards + i
-            spacesUsed = math.max(spacesUsed, activeCards.size)
-        })
-        spacesUsed
+        shuffledDeck.foldLeft(State()) { (acc, i) =>
+            val activeCards = if (acc.activeCards(i)) acc.activeCards - i else acc.activeCards + i
+            val spacesUsed = math.max(acc.spacesUsed, activeCards.size)
+            State(activeCards, spacesUsed)
+        }.spacesUsed
     }
 
     def play(): Boolean = {
